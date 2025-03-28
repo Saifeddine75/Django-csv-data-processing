3659a9eb896ce2d1a13b2ca6daca0b1e4aabdbd5
Saifeddine <saifeddine.bejaoui@gmail.com>
2025-03-28 18:28:50 +0100

    NEW feature: Plot dataset

--- backend/visualizer/urls.py
+++ backend/visualizer/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
+
+from datasets import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
+    path('datasets/', views.FileUploadView.as_view(), name='upload-from')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
