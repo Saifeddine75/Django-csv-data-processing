384d9f484af6ade59503f43a253c898f9f1e8965
Saifeddine75 <saifeddine.bejaoui@gmail.com>
2025-03-31 22:02:21 +0200

    Show dataset plot

--- backend/visualizer/settings.py
+++ backend/visualizer/settings.py
@@ -128,6 +128,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Collect all static files here when running collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
+# Include app-level static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'visualizer/static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
