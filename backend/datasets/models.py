c8a4de0eab8bd8b85468bac050eb36a8f5df31f4
Saifeddine75 <saifeddine.bejaoui@gmail.com>
2025-03-31 18:53:35 +0200

    NEW APP: datasets

---
+++ backend/datasets/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+# TODO: use ArrayField
+# from django.contrib.po
+# stgres.fields import ArrayField
+
+class Dataset(models.Model):
+    title = models.CharField(max_length=255, default=timezone.now)  # To store the title of the chart
+    timestamp = models.JSONField(models.FloatField(), default=list)  # To store the timestamp values
+    x = models.JSONField(models.FloatField(), default=list)
+    y = models.JSONField(models.FloatField(), default=list)
+    z = models.JSONField(models.FloatField(), default=list)
+    norm = models.JSONField(models.FloatField(), default=list)
+    stats = models.JSONField(default=dict)
+    created_at = models.DateTimeField(default=timezone.now) 
+
+    def __str__(self):
+        return f"Data from {self.timestamp} (x: {self.x}, y: {self.y}, z: {self.z})"
+    
+
+class DatasetChart(models.Model):
+    title = models.CharField(max_length=255, default=timezone.now)  # To store the title of the chart
+    plot_data = models.JSONField(default=dict)  # To store the chart data as a JSON object
+    stats = models.JSONField(default=dict)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title  # Display the title in the admin panel or elsewhere
---
+++ backend/datasets/views.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+import json
+
+from django.contrib import messages
+from django.views.generic.edit import FormView
+
+from datasets.models import Dataset, DatasetChart
+
+# from .forms import FileFieldForm
+from .forms import FileUploadForm
+from utils.helpers.handlers import handle_dataset_file
+
+
+from django.shortcuts import get_object_or_404, redirect
+from django.http import JsonResponse
+from .models import Dataset  # Ensure you import your Dataset model
+# Create your views here.
+
+
+
+class DatasetUploadView(FormView):
+    template_name = "pages/dataset_plot.html"
+    form_class = FileUploadForm
+    success_url = "."
+
+    def form_valid(self, form):
+        uploaded_file = self.request.FILES["file"]
+        handle_dataset_file(uploaded_file)
+        messages.success(self.request, f"File '{uploaded_file.name}' uploaded successfully!")
+        return super().form_valid(form)
+
+    def compute_dataset_chart(self, title:str, dataset_id:str=None):
+        """Compute chart for the given dataset_id or the latest dataset."""
+        if not dataset_id:
+            dataset = Dataset.objects.last()
+        else:
+            dataset = Dataset.objects.filter(id=dataset_id).first()
+
+        stats = dataset.stats
+        plot_data = {
+            "title": title,
+            "timestamps": dataset.timestamp,
+            "series": [
+                {"name": "X", "data": list(zip(dataset.timestamp, dataset.x))},
+                {"name": "Y", "data": list(zip(dataset.timestamp, dataset.y))},
+                {"name": "Z", "data": list(zip(dataset.timestamp, dataset.z))},
+                {"name": "Norm", "data": list(zip(dataset.timestamp, dataset.norm))}
+            ],
+            'stats': stats
+        }
+        DatasetChart(
+            title=title,
+            plot_data=plot_data,
+            stats=stats,
+            created_at=datetime.now()
+        )
+        return plot_data
+
+    def delete_dataset(self, dataset_id:int=None):
+        if dataset_id:
+            dataset = Dataset.objects.filter(id=dataset_id).first()
+            dataset.delete()
+            messages.success(self.request, f"Dataset {dataset_id} deleted successfully!")
+        return redirect("upload_dataset")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        all_datasets = Dataset.objects.all()
+
+        selected_dataset_id = self.request.GET.get("dataset_id")
+        selected_dataset = Dataset.objects.filter(id=selected_dataset_id).first()
+
+        chart_data = self.compute_dataset_chart(title="dataset-{selected_dataset_id}", dataset_id=selected_dataset_id) if selected_dataset else None
+
+        context.update({
+            "datasets": all_datasets,
+            "selected_dataset": selected_dataset,
+            "chart_data": json.dumps(chart_data) if chart_data else None,
+            "stats": chart_data.get('stats') if chart_data else None
+        })
+        return context
+    
+
+        
+    
+
+# def delete_dataset(request, id:int):
+#     if request.method == "POST":
+#         dataset = get_object_or_404(Dataset, id=id)
+
+#         dataset.delete()  # Delete dataset from the database
+#         messages.success(request, f"Dataset {id} deleted successfully!")
+
+
