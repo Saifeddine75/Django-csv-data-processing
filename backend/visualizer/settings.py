0b062fba8d45431dd8d9da3d862cf053b36ad6f5
Saifeddine75 <saifeddine.bejaoui@gmail.com>
2025-04-02 23:56:26 +0200

    fix debug toolbar

--- backend/visualizer/settings.py
+++ backend/visualizer/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from dotenv import load_dotenv
 from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
@@ -41,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    
-    # My Apps
+
+    # My apps
     'datasets'
 ]
 
@@ -126,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Collect all static files here when running collectstatic
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
@@ -135,9 +136,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'visualizer/static'),
 ]
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
