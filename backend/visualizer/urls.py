536cf532025d6d4516e40eb04466cd6360cb0b02
Saifeddine75 <saifeddine.bejaoui@gmail.com>
2025-04-07 00:36:47 +0200

    Add root endpoint redirection

--- backend/visualizer/urls.py
+++ backend/visualizer/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/datasets/', permanent=False)),
     path('admin/', admin.site.urls),
     path('datasets/', include('datasets.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
